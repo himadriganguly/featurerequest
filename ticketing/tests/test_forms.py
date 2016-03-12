08fab6a64e0f303d1c053f7956d51fee8717f904
Himadri Ganguly <himadri@colourdrift.com>
2016-03-12 14:51:29 +0530

    Added unit testing for forms.py

---
+++ ticketing/tests/test_forms.py
@@ -0,0 +1,78 @@
+from django.test import TestCase
+from django.core.exceptions import NON_FIELD_ERRORS
+from ticketing.factories import ClientFactory, ProductFactory, FeatureRequestFactory
+from ticketing.forms import FeatureRequestForm
+
+# Create your tests here.
+
+class FeatureRequestTest(TestCase):
+	
+	def setUp(self):
+		self.client = ClientFactory()
+		self.product = ProductFactory()
+		self.new_feature_request = FeatureRequestFactory()
+	
+	def test_valid_form(self):			
+		form = FeatureRequestForm(data={
+			'title': "This is a test title",
+			'desc': self.new_feature_request.desc,
+			'targetdate': '03/24/2016',
+			'url': 'http://www.example.com',	
+			'client': self.client.pk,
+			'productarea': self.product.pk,
+		})
+		
+		self.assertTrue(form.is_valid())
+	
+	def test_no_title(self):
+		form = FeatureRequestForm(data={
+			'title': '',
+			'desc': self.new_feature_request.desc,
+			'targetdate': '03/24/2016',
+			'url': 'http://www.example.com',	
+			'client': self.client.pk,
+			'productarea': self.product.pk,
+		})
+		
+		self.assertFalse(form.is_valid())
+		self.assertTrue(form.has_error('title', code='required'))	
+		
+	def test_no_desc(self):
+		form = FeatureRequestForm(data={
+			'title': 'This is a test title',
+			'desc': '',
+			'targetdate': '03/24/2016',
+			'url': 'http://www.example.com',	
+			'client': self.client.pk,
+			'productarea': self.product.pk,
+		})
+		
+		self.assertFalse(form.is_valid())
+		self.assertTrue(form.has_error('desc', code='required'))
+	
+	def test_no_targetdate(self):
+		form = FeatureRequestForm(data={
+			'title': 'This is a test title',
+			'desc': self.new_feature_request.desc,
+			'targetdate': '',
+			'url': 'http://www.example.com',	
+			'client': self.client.pk,
+			'productarea': self.product.pk,
+		})
+		
+		self.assertFalse(form.is_valid())
+		self.assertTrue(form.has_error('targetdate', code='required'))	
+	
+	def test_no_client(self):
+		form = FeatureRequestForm(data={
+			'title': 'This is a test title',
+			'desc': self.new_feature_request.desc,
+			'targetdate': '',
+			'url': 'http://www.example.com',	
+			'client': '',
+			'productarea': self.product.pk,
+		})
+		
+		self.assertFalse(form.is_valid())
+		self.assertTrue(form.has_error('client', code='required'))	
+	
